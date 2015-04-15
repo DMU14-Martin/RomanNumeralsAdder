e91d3f8b720d2a180f09ac41610995227997d2a6
DMU14-Martin <M.skjoedt@live.dk>
2015-04-15 21:49:39 +0200

    refactored, init method added for test, testmethod now uses constants for testcase input, expected output and actual output. Added service method to validate input in implementation

--- src/RomanNumeralsAdder.java
+++ src/RomanNumeralsAdder.java
@@ -2,10 +2,20 @@
 public class RomanNumeralsAdder {
 
 	public String add(String inputA, String inputB) {
-		if(inputA == "I" && inputB == "I")
+		if(validator(inputA) && validator(inputB))
 			return "II";
 		else
 			return "Ugyldig input";
+	}
+	
+	
+	private boolean validator(String input){		
+	
+		if(input == "I"){
+			return true;
+		}
+		else
+			return false;
 	}
 
 }
--- test/testRomanNumeralAdder.java
+++ test/testRomanNumeralAdder.java
@@ -1,21 +1,47 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class testRomanNumeralAdder {
+	
+	private RomanNumeralsAdder romAdd;
+
+	@Before
+	public void init(){
+		romAdd = new RomanNumeralsAdder();
+	}
 
 	//testcase 1
 	@Test
-	public void EmptyInputStringAndIequalsUgyldigInput() {
-		RomanNumeralsAdder romAdd = new RomanNumeralsAdder();
-		assertEquals("Ugyldig input", romAdd.add("","I"));
+	public void emptyInputStringAndIequalsUgyldigInput() {
+		//input
+		final String inputA = "";
+		final String inputB = "I";
+		
+		//expected output
+		final String expected = "Ugyldig input";
+		
+		//actual output
+		final String actual = romAdd.add(inputA, inputB);
+		
+		assertEquals(expected, actual);
 	}
 	
 	//testcase 2
 	@Test
 	public void inputIandIequalsII(){
-		RomanNumeralsAdder romAdd = new RomanNumeralsAdder();
-		assertEquals("II", romAdd.add("I","I"));
+		//input
+		final String inputA = "I";
+		final String inputB = "I";
+		
+		//expected output
+		final String expected = "II";
+		
+		//actual output
+		final String actual = romAdd.add(inputA, inputB);
+		
+		assertEquals(expected, actual);
 	}
 }
