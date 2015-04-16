c3bbbce0962c025c4bc8f5835543f9a1e3346ad6
DMU14-Martin <M.skjoedt@live.dk>
2015-04-16 17:11:05 +0200

    refactored, regular expression added for validator.

--- src/RomanNumeralsAdder.java
+++ src/RomanNumeralsAdder.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class RomanNumeralsAdder {
 
@@ -11,20 +14,20 @@ public class RomanNumeralsAdder {
 	
 	private String formatter(String inputA, String inputB) {
 		String temp = inputA + inputB;
-		if(temp.contains("IIIII")){
-				temp = "V";
-			}
+		temp = temp.replace("IIIII", "V");
 		return temp;
 	}
 
 
 	private boolean validator(String input){		
-	
-		if(input == "I" || input == "II" || input == "III"){
-			return true;
+		String regex = "^$";
+		Pattern checkRegex = Pattern.compile(regex);
+		Matcher regexMatcher = checkRegex.matcher(input);
+		
+		if(regexMatcher.find()){
+			return false;
 		}
 		else
-			return false;
+			return true;
 	}
-
 }
